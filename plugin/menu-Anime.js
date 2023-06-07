6ad1a261c9fc77a1f7364a6c443b4f9f36154e99
OFC HADES-BOT-MD <100550974+OFC-YOVANI@users.noreply.github.com>
2023-06-07 13:08:18 -0600

    🧿©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰🪀

---
+++ plugin/menu-Anime.js
@@ -0,0 +1,93 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
+try {
+await m.reply(`╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」\n│➯(♦️)*Hola estás en el menu-Anime elige una opción y pide una imagen*\n╰───────────────╯\n\n\n╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」\n│➯(♦️)*Hello you are in the menu-Anime choose an option and ask for an image*\n╰───────────────╯`)
+let pp = imagen5
+let vn = './Audios/Konede.mp3'
+let img = await(await fetch('https://i.imgur.com/JP52fdP.jpg')).buffer()
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let user = global.db.data.users[m.sender]
+let { money, joincount } = global.db.data.users[m.sender]
+let { exp, limit, level, role } = global.db.data.users[m.sender]
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let str = `╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」
+│➯𝗛ᴏʟᴀ,➟${taguser}
+│➯ᴏᴡɴᴇʀ:ᴏғᴄ➟ʏᴏᴠᴀɴɪ
+│➯*Número*➟5212412377467
+│➯*canal-YouTube*
+│➯https://youtube.com/@hades_bot2391
+│➯ғᴇᴄʜᴀ:➟${date}
+│➯ᴛɪᴇᴍᴘᴏ ᴀᴄᴛɪᴠᴏ:➟${uptime}
+│➯ᴜsᴜᴀʀɪᴏs:➟${rtotalreg}
+︎╰───────────────╯
+🍁᭢━━━━━━━━━᭥🍁᭢
+╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」
+│➯ *Menu-Anime*
+︎╰───────────────╯
+🍁᭢━━━━━━━━━᭥🍁᭢
+╭─「➻❥ *Animes* ➻❥」
+│➯ *.miku*
+│➯ *.akira*
+│➯ *.akiyama*
+│➯ *.anna*
+│➯ *.asuna*
+│➯ *.ayuzawa*
+│➯ *.boruto*
+│➯ *.chiho*
+│➯ *.chitoge*
+│➯ *.deidara*
+│➯ *.erza*
+│➯ *.elaina*
+│➯ *.eba*
+│➯ *.emilia*
+│➯ *.hestia*
+│➯ *.hinata*
+│➯ *.inori*
+│➯ *.isuzu*
+│➯ *.itachi*
+│➯ *.itori*
+│➯ *.kaga*
+│➯ *.kagura*
+│➯ *.kaori*
+│➯ *.keneki*
+│➯ *.kotori*
+│➯ *.kurumi*
+│➯ *.madara*
+│➯ *.mikasa*
+│➯ *.minato*
+│➯ *.naruto*
+│➯ *.nezuko*
+│➯ *.sagiri*
+│➯ *.sasuke*
+│➯ *.sakura*
+│➯ *.cosplay*
+│➯⫷᭄©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰✍
+╰───────────────╯`.trim()
+if (m.isGroup) {
+await conn.sendFile(m.chat, vn, 'Konede.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
+conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })  
+} else {    
+await conn.sendFile(m.chat, vn, 'Konede.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
+conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })}
+} catch {
+conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
+}}
+handler.command = /^(Animes|animes)$/i
+handler.exp = 50
+handler.fail = null
+export default handler
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
---
+++ plugin/owner-banchat.js
@@ -0,0 +1,9 @@
+let handler = async (m) => {
+global.db.data.chats[m.chat].isBanned = true
+m.reply('*│‼️│ 🗝️𝚎𝚜𝚝𝚎 𝚌𝚑𝚊𝚝 𝚋𝚊𝚗𝚎𝚊𝚍𝚘 𝚎𝚡𝚒𝚝𝚘𝚜𝚊𝚖𝚎𝚗𝚝𝚎🗝️*\n\n\n*│‼️│THIS CHAT WAS SUCCESSFULLY BANNED🗝*')
+}
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^banchat$/i
+handler.rowner = true
+export default handler
