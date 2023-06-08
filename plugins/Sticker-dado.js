a657d303498d480b80a400b2c7a342d995861961
OFC HADES-BOT-MD <100550974+OFC-YOVANI@users.noreply.github.com>
2023-06-07 18:50:09 -0600

    🧿©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰🪀

--- plugins/Sticker-dado.js
+++
@@ -1,5 +0,0 @@
-let handler = async (m, { conn }) => {
-let dir = 'https://api.lolhuman.xyz/api/sticker/dadu?apikey=85faf717d0545d14074659ad'
-conn.sendFile(m.chat, dir, 'error.webp', null, m, { asSticker: true })}
-handler.command = ['dado', 'dados', 'dadu'] 
-export default handler
