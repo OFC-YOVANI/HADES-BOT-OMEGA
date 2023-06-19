55dd0e0ee0b0cd5bb5916a100773270add37ec81
OFC HADES-BOT-MD <100550974+OFC-YOVANI@users.noreply.github.com>
2023-06-19 12:02:25 -0600

    🧿©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰🪀

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -9,18 +9,22 @@ try {
 pp = await conn.profilePictureUrl(who)
 } catch (e) {
 } finally {
-let { name, limit, lastclaim, registered, regTime, age, premiumTime } = global.db.data.users[who]
+let { name, role, level, limit, money, exp, joincount, lastclaim, registered, regTime, age, premiumTime } = global.db.data.users[who]
 let username = conn.getName(who)
 let prem = global.prems.includes(who.split `@` [0])
 let sn = createHash('md5').update(who).digest('hex')
 let str = `╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」
 │➯ *𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + ') ': ''}
-│➯ *𝙽𝚄𝙼𝙴𝚁𝙾:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-│➯ *𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
-│➯ *𝙻𝙸𝙼𝙸𝚃𝙴:* ${limit} 𝚄𝚂𝙾𝚂
-│➯ *𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾:* ${registered ? 'Si': 'No'}
-│➯ *𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
-│➯ *𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
+│➯ *🌐𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
+│➯ *Rango:* ${role}
+│➯ *✳️️Nivel:* ${level}
+│➯ *🧿Experiencia:* ${exp}
+│➯ *💎Diamantes:* ${limit} 𝚄𝚂𝙾𝚂
+│➯ *👾Hades-Coins:* ${money}
+│➯ *🪙Tokens:* ${joincount}
+│➯ *💥𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾:* ${registered ? 'Si': 'No'}
+│➯ *✔️𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
+│➯ *📝𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
 │➯ *${sn}*
 ╰───────────────╯`
 conn.sendMessage(m.chat, { image: { url: pp }, caption: str }, { quoted: m })
