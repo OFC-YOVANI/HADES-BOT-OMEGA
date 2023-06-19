12b5c0c71d0bcffae7169b56767e85aafc2863a0
OFC HADES-BOT-MD <100550974+OFC-YOVANI@users.noreply.github.com>
2023-06-19 12:26:28 -0600

    🧿©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰🪀

--- plugins/YouTube-mediafire.js
+++ plugins/YouTube-mediafire.js
@@ -1,51 +1,55 @@
+import { mediafiredl } from '@bochilteam/scraper'
 import axios from 'axios'
 import fetch from 'node-fetch'
-import cheerio from 'cheerio'
-
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-   if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/h9x1bgb79vmllkn/OFC-HADES-BOT.zip*\n\n\n*[❗𝐈𝐍𝐅𝐎❗] ENTER A VALID MEDIAFIRE LINK EXAMPLE* *${usedPrefix + command} https://www.mediafire.com/file/h9x1bgb79vmllkn/OFC-HADES-BOT.zip*`
-   try {  
-      let res = await mediafireDl(args[0])
-      let { name, size, date, mime, link } = res
-      let caption = `----------------------------------------------------------------------------------------------------------
-𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰
-➯*📓 𝙽𝙾𝙼𝙱𝚁𝙴:* ${name}
-➯*📁 𝙿𝙴𝚂𝙾:* ${size}
-➯*📄 𝚃𝙸𝙿𝙾:* ${mime}
-
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/h9x1bgb79vmllkn/OFC-HADES-BOT.zip/file*`
+try {
+let json = await mediafireDl2(args[0])
+let caption = `╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」
+│🍀*𝙽𝙾𝙼𝙱𝚁𝙴:*  ${json[0].nama.replace('+', ' ')}
+│💎*𝙿𝙴𝚂𝙾:* ${json[0].size}
+│🌴*𝚃𝙸𝙿𝙾:* ${json[0].mime.toUpperCase()}
+╰─────°.♡.°‧─────
 *⏳ 𝙴𝚂𝙿𝙴𝚁𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾. . . .* 
-
-----------------------------------------------------------------------------------------------------------
-
-𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰
-➯*📓 NAME :* ${name}
-➯*📁 WEIGHT:* ${size}
-➯*📄 GUY:* ${mime}
-
-*⏳WAIT WHEN WE SEND YOUR FILE.....`.trim()
-   await m.reply(caption)
-   await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: mime, asDocument: true })
-   } catch {  
-      await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://www.mediafire.com/file/h9x1bgb79vmllkn/OFC-HADES-BOT.zip/file*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]ERROR TRY AGAIN CHECK IF THE LINK IS SIMILAR\n*◉ https://www.mediafire.com/file/h9x1bgb79vmllkn/OFC-HADES-BOT.zip/file*')
-   }
-}
-
+`.trim()   
+conn.sendMessage(m.chat, { text: caption, footer: wm }, { quoted: m })
+conn.sendMessage(m.chat, { document : { url: json[0].link }, fileName : json[0].nama.replace('+', ' '), mimetype: json[0].mime.toUpperCase() }, { quoted: m })
+} catch {  
+try {  
+let res = await mediafiredl(args[0])
+let { url, url2, filename, ext, aploud, filesize, filesizeH } = await res
+let caption = `╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」
+│🍀*𝙽𝙾𝙼𝙱𝚁𝙴:* ${filename}
+│💎*𝙿𝙴𝚂𝙾:* ${filesizeH}
+│🌴*𝚃𝙸𝙿𝙾:* ${ext}
+╰─────°.♡.°‧─────
+*⏳ 𝙴𝚂𝙿𝙴𝚁𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾. . . .* 
+`.trim()
+let msg = m.reply(caption)
+if (!msg || msg == '') return m.reply(global.wait)
+let res2 = await fetch(`https://api.lolhuman.xyz/api/mediafire?apikey=${lolkeysapi}&url=${args[0]}`)
+let res3 = await res2.json()
+let res4 = await res3.result.link  
+if (!url || url == '') url = res4
+await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+} catch {  
+await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://www.mediafire.com/file/h9x1bgb79vmllkn/OFC-HADES-BOT.zip/file*')
+}}}
 handler.help = ['mediafire'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i
 handler.register = true
 handler.limit = 4
 export default handler
-
-async function mediafireDl(url) {
-   const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/','')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`)
-   const $ = cheerio.load(res.data)
-   const link = $('#downloadButton').attr('href')
-   const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ','').replaceAll('\n','')
-   const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text()
-   const size = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ','')
-   let mime = ''
-   let rese = await axios.head(link)
-   mime = rese.headers['content-type']
-   return { name, size, date, mime, link }
-}
+async function mediafireDl2(url) {
+const res = await axios.get(url) 
+const $ = cheerio.load(res.data)
+const hasil = []
+const link = $('a#downloadButton').attr('href')
+const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('', '')
+const seplit = link.split('/')
+const nama = seplit[5]
+let mime = nama.split('.')
+mime = mime[1]
+hasil.push({ nama, mime, size, link })
+return hasil }     
