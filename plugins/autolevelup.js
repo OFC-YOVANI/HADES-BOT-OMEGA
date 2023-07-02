3334401dd7d4c432300c91b1d7865177048c2810
OFC HADES-BOT-MD <100550974+OFC-YOVANI@users.noreply.github.com>
2023-07-02 14:45:57 -0600

    🧿©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰🪀

--- plugins/autolevelup.js
+++ plugins/autolevelup.js
@@ -5,11 +5,12 @@ export function before(m, { conn }) {
 let user = global.db.data.users[m.sender]
 if (!user.autolevelup) 
   return !0
-	
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }	
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
  if (before !== user.level) {
-m.reply(`╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」\n│➯ 🥳 ғᴇʟɪᴄɪᴅᴀᴅᴇs ʜᴀ sᴜʙɪᴅᴏ ᴅᴇ ɴɪᴠᴇʟ\n│➯🔸ɴɪᴠᴇʟ ᴀɴᴛᴇʀɪᴏʀ: ${before} \n│➯ ⬆️ɴɪᴠᴇʟ ᴀᴄᴛᴜᴀʟ: ${user.level} \n│➯ ⚔️ʀᴀɴɢᴏ: ${user.role} \n│➯ 📅ғᴇᴄʜᴀ: ${new Date().toLocaleString('id-ID')} \n╰───────────────╯`.trim()) 
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+m.reply(`╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」\n│➯ *𝗛ᴏʟᴀ,➟${taguser} \n│➯ 🥳 ғᴇʟɪᴄɪᴅᴀᴅᴇs ʜᴀ sᴜʙɪᴅᴏ ᴅᴇ ɴɪᴠᴇʟ\n│➯🔸ɴɪᴠᴇʟ ᴀɴᴛᴇʀɪᴏʀ: ${before} \n│➯ ⬆️ɴɪᴠᴇʟ ᴀᴄᴛᴜᴀʟ: ${user.level} \n│➯ ⚔️ʀᴀɴɢᴏ: ${user.role} \n│➯ 📅ғᴇᴄʜᴀ: ${new Date().toLocaleString('id-ID')} \n╰───────────────╯`.trim()) 
 }}
 
  
