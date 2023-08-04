fb591ee2f33ace6d6a50776200bcaf1821d11dde
OFC HADES-BOT-MD <100550974+OFC-YOVANI@users.noreply.github.com>
2023-08-04 12:43:44 -0600

    🧿©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰🪀

--- main.js
+++ main.js
@@ -111,7 +111,7 @@ return { conversation: "hello, i'm OFC-YOVANI" }},
 msgRetryCounterMap,
 logger: pino({ level: 'silent' }),
 auth: state,
-browser: ['HADES-BOT-OMEGA','Safari','9.7.0'],
+browser: ['HADES-BOT-OMEGA','Safari','1.0.0'],
 version,
 defaultQueryTimeoutMs: undefined    
 }
@@ -127,24 +127,6 @@ if (opts['autocleartmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 't
 
 if (opts['server']) (await import('./server.js')).default(global.conn, PORT)
 
-   
-     /* Y ese fue el momazo mas bueno del mundo
-        Aunque no dudara tan solo un segundo
-        Mas no me arrepiento de haberme reido
-        Por que la grasa es un sentimiento
-        Y ese fue el momazo mas bueno del mundo
-        Aunque no dudara tan solo un segundo
-        que me arrepiento de ser un grasoso
-        Por que la grasa es un sentimiento
-        - El waza 👻👻👻👻 (Aiden)            */
-   
-   /* Yo tambien se hacer momazos Aiden... 
-      ahi te va el ajuste de los borrados 
-      inteligentes de las sesiones y de los sub-bot  
-      By (Rey Endymion 👺👍🏼) */
-/*ninguno es mejor que tilin god 
-atte: sk1d*/
-       
 function clearTmp() {
 const tmp = [tmpdir(), join(__dirname, './tmp')]
 const filename = []
