474bebc75bfd3f870f96e592dc87df558cb487ec
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-06-10 20:12:09 -0600

    ©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰/©ᴏғᴄ-ʏᴏᴠᴀɴɪ

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ import {Boom} from '@hapi/boom'
 import {makeWASocket, protoType, serialize} from './lib/simple.js'
 import {Low, JSONFile} from 'lowdb'
 import {mongoDB, mongoDBV2} from './lib/mongoDB.js'
-import store from './store.js'
+import store from './lib/store.js'
 import { hutaoJadiBot } from './plugins/jadibot-serbot.js';
 const {proto} = (await import('@whiskeysockets/baileys')).default
 import pkg from 'google-libphonenumber'
